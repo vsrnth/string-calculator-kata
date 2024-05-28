451319d58b1de87dba57996729b5ea876304d603
Srinath <vsrinath013@gmail.com>
2024-05-28 16:05:04 +0530

    Adds tests for cases where different delimiters are passed
    - Test for scenario when the delimiter used in the input string is passed in a line before the string
    - Test for scenario when the delimiter used in the input string is not passed in a line before the string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -14,4 +14,11 @@ describe StringCalculator do
   it 'should return sum of the numbers in string, if the numbers in string have new line delimiters' do
     expect(calculator.add("1\n2,3")).to eq(6)
   end
+  it 'should return sum of the numbers in string, the delimiters are passed in a line before the string' do
+    expect(calculator.add("//;\n12;2")).to eq(14)
+  end
+
+  it 'should return sum of the numbers in string, the delimiters are not passed in a line before the string' do
+    expect(calculator.add("12;2")).to eq(14)
+  end
 end
