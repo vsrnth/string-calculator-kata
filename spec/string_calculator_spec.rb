516bc178569f4665b6d2c2dc8adfe4f36cc0bdd4
Srinath <vsrinath013@gmail.com>
2024-05-28 15:35:01 +0530

    Adds basic implementation of StringCalculator
    - splits input string into an array, returns 0 if empty
    - fixes failing unit test

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 require 'rspec'
+require_relative '../src/string_calculator'
 
 describe StringCalculator do
   subject(:calculator) { described_class.new }
---
+++ src/string_calculator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class StringCalculator
+  def add(numbers)
+    number_array = numbers.split(',')
+    0 if number_array.empty?
+  end
+end
