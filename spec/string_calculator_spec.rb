0c4386a28ac526ebc7d2b8bc06e9d7c987e3b980
Srinath <vsrinath013@gmail.com>
2024-05-28 15:43:22 +0530

    Adds test for adding numbers from string with commas

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,4 +8,7 @@ describe StringCalculator do
   it 'should return 0 for an emptry string input' do
     expect(calculator.add('')).to eq(0)
   end
+  it 'should return sum of the numbers in string, if the numbers in string are separated by comma' do
+    expect(calculator.add('1,2,3')).to eq(6)
+  end
 end
