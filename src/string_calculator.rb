9a9be532eaa9ee3d72b17535657749511364327f
Srinath <vsrinath013@gmail.com>
2024-05-28 18:02:22 +0530

    Refactored the negative number handling to make rubocop happy

--- src/string_calculator.rb
+++ src/string_calculator.rb
@@ -3,18 +3,16 @@
 class StringCalculator
   def add(numbers)
     numbers_array = numbers.scan(/[0-9-]+/).map(&:to_i)
-    raise "negatives not allowed - #{list_negative_numbers(numbers_array)}" if list_negative_numbers(numbers_array)
+    negative_numbers = list_negative_numbers(numbers_array)
+    raise "negatives not allowed - #{negative_numbers}" if negative_numbers
+
     numbers_array.reduce(0) { |sum, i| sum + i }
   end
 
   private
 
   def list_negative_numbers(numbers_array)
-    negative_numbers = numbers_array.select { |num| num < 0}
-    if negative_numbers.size > 0
-      return negative_numbers.join(",")
-    else
-      return false
-    end
+    negative_numbers = numbers_array.select(&:negative?)
+    negative_numbers.join(',') if negative_numbers.size.positive?
   end
 end
