3e3b2f939e69013303e3b194028eedb127908608
Srinath <vsrinath013@gmail.com>
2024-05-28 15:55:04 +0530

    Enhance add method to support newlines and commas as delimiters

--- src/string_calculator.rb
+++ src/string_calculator.rb
@@ -2,7 +2,7 @@
 
 class StringCalculator
   def add(numbers)
-    number_array = numbers.split(',').map(&:to_i)
+    number_array = numbers.gsub("\n", ",").split(',').map(&:to_i)
     number_array.sum
   end
 end
